3f7809125affefcdea84733ba72c85f8888d9765
André Paes <aluispaes@gmail.com>
2022-01-15 19:25:27 -0300

    Project created to show how to compare if the same file exists into distinct folders

---
+++ listdir.py
@@ -0,0 +1,57 @@
+'''
+Contexto:
+1. Compactar arquivos do diretório Original através de software específico.
+2. Este software, após compactar os arquivos, cria um novo diretório (chamado Zip)
+3. Os arquivos do diretório Zip possuem o mesmo nome do diretório Original, com adição do sufixo _zip.
+    Exemplo: Nome de um arquivo no diretório Original: picture.jpg  => após ser compactado, o arquivo tem o nome
+    picture_zip.jpg
+4. Para conferir se todos os arquivos da pasta realmente foram compactados, foi desenvolvida a lógica a seguir.
+'''
+from os import listdir  #pecorrer os arquivos do diretório
+from os.path import isfile, join    #confirmar se é um arquivo
+
+#simulando diretório Original e o diretório Zip
+OriginalPath = 'C:/Users/aluis/PycharmProjects/listDir'
+ZipPath = 'C:/Users/aluis/PycharmProjects/listDir'
+
+#ler cada item do diretório e se for um arquivo, adiciona à list[]
+OriginalFiles = [f for f in listdir(OriginalPath) if isfile(join(OriginalPath, f))]
+ZipFiles = [f for f in listdir(ZipPath) if isfile(join(ZipPath, f))]
+
+#burlando um arquivo não compactado (contém no dir Original, mas não contém no dir Zip)
+OriginalFiles.append('fake.py')
+
+#listar o nome dos arquivos (diretório Original) sem a extensão .py
+OriginalName = []
+for file in OriginalFiles:
+    OriginalName.append(file[:-3]) #removendo a extensão .py
+
+#listar o nome dos arquivos (diretório Zip) sem a extensão .py
+ZipListName = []
+for file in ZipFiles:
+    ZipListName.append(file[:-3])
+
+#lista criada para manipular durante a comparação
+zipList = ZipListName.copy()
+
+#simular: renomear o nome dos arquivos compactados (armazenados na pasta Zip)
+for i in range(0, len(ZipListName)):
+    original = ZipListName[i]
+    novo = original + '_zip.py'
+    ZipListName[i] = novo
+
+#lógica de comparação entre os diretórios (Original X Zip)
+filesZip = []
+filesNoZip = OriginalName.copy() #Lista p/ identificar arquivos não compactados (cópia da lista Original para fazer 'engenharia reversa')
+i = 0
+for file in OriginalName:   #para cada arquivo no diretório Original
+    for i in range(0, len(zipList)):    #percorrer cada arquivo no diretório Zip
+        if file == zipList[i]:          #se nome do arquivo Original é igual ao nome do arquivo Zip
+            filesZip.append(file)    #copiar o nome do arquivo para Lista de Arquivos Compactados (lista Zip)
+
+for file in filesZip:    #para cada arquivo na lista recém criada de Arquivos Copiados
+    filesNoZip.remove(file)  #engenharia reversa: remover da lista de não compactados o nome do arquivo que está na lista de compactados
+
+print(f'Original Files: {OriginalFiles}')
+print(f'Files copied: {filesZip}')
+print(f'Files no copied: {filesNoZip}')
